360a55e70e922e92ba8b2ff37ddf2de177d5c304
Shanu Chandra Ghosh <166943936+Shanu-Ch-Ghosh@users.noreply.github.com>
2024-08-02 19:13:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+
+let arr = [1,2,3,4]
+
+let oddNum = arr.filter((val)=>{
+	return val%2!=0;
+});
+
+
+
+let evenNum = arr.filter((val)=>{
+	return val%2==0;
+}).map((val)=>{
+	return val*2;
+})
+
+
+
+let outputBox = document.getElementById("output");
+
+let promise = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve(arr);
+	},3000)
+});
+
+promise.then((arr)=>{
+	outputBox.innerText = (arr);
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+		resolve(oddNum);
+	   },1000)
+	})
+})
+.then((oddNum)=>{
+	outputBox.innerText = (oddNum);
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+		resolve(evenNum);
+	   },2000)
+	})
+})
+.then((evenNum)=>{
+	outputBox.innerText = (evenNum);
+})
